ac47cca477c859c230a960b2076255a8789da8d3
Daniel <daniel.desarrolloweb@outlook.com>
2020-11-30 17:50:19 -0300

    Intents parte 1
    Intents parte 1

--- app/src/main/java/com/example/myapplication/activities/IntentsActivity.kt
+++ app/src/main/java/com/example/myapplication/activities/IntentsActivity.kt
@@ -1,12 +1,36 @@
 package com.example.myapplication.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.R
+import kotlinx.android.synthetic.main.activity_intents.*
 
 class IntentsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intents)
+
+        buttonIntentExtras.setOnClickListener { goIntentExtras() }
+        buttonIntentFlags.setOnClickListener { goIntentFlags() }
+        buttonIntentObject.setOnClickListener { goIntentObject() }
     }
+
+    private fun goIntentExtras(){
+        val intent = Intent(this, IntentsActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun goIntentFlags(){
+        val intent = Intent(this, IntentsActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun goIntentObject(){
+        val intent = Intent(this, IntentsActivity::class.java)
+        startActivity(intent)
+    }
+
+
+
 }
