dff4c0ee7baf5cc26e3e5c6da95af5744c994052
Daniel <daniel.desarrolloweb@outlook.com>
2020-11-30 17:13:21 -0300

    listView en pantalla con adaptador
    listView en pantalla con adaptador personalizado

--- app/src/main/java/com/example/myapplication/activities/ListViewActivity.kt
+++ app/src/main/java/com/example/myapplication/activities/ListViewActivity.kt
@@ -3,10 +3,31 @@ package com.example.myapplication.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.R
+import com.example.myapplication.adapters.PersonAdapter
+import com.example.myapplication.models.Person
+import kotlinx.android.synthetic.main.activity_list_view.*
 
 class ListViewActivity : AppCompatActivity() {
+
+    private lateinit var adapter: PersonAdapter
+    private lateinit var personList: List<Person>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_view)
+
+        personList = getPerson()
+        adapter = PersonAdapter(this,R.layout.list_view_person,personList)
+
+        listView.adapter = adapter
+    }
+
+    private fun getPerson(): List<Person> {
+        return listOf(
+            Person("Daniel","Urra",33)
+            ,Person("Denis","Roman",33)
+            , Person("Joaquin","Valenzuela",9)
+
+        )
     }
 }
---
+++ app/src/main/java/com/example/myapplication/models/Person.kt
@@ -0,0 +1,5 @@
+package com.example.myapplication.models
+
+data class Person (val firstName: String, val lastName: String, val age: Int){
+
+}
