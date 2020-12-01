07d580f43ee3e9bbadecd2a719322871004156a1
Daniel <daniel.desarrolloweb@outlook.com>
2020-12-01 13:39:48 -0300

    tomar fotos con la camara con persmisos

--- app/src/main/java/com/example/myapplication/activities/PermisionsActivity.kt
+++ app/src/main/java/com/example/myapplication/activities/PermisionsActivity.kt
@@ -1,13 +1,113 @@
 package com.example.myapplication.activities
 
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.myapplication.R
+import kotlinx.android.synthetic.main.activity_permisions.*
+
+
 
 class PermisionsActivity : AppCompatActivity() {
+
+    private val requestCameraPermission = 100
+    private val requestCameraPicture = 200
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_permisions)
+
+        buttonPicture.setOnClickListener({ getPictureFromCameraAskingPermissions() })
+    }
+
+    private fun getPictureFromCameraAskingPermissions() {
+        //añadir permiso al manifest
+        //comprobar el permiso d ela camara
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // se no ha sido aceptado previamente (para version desde la 6.0 [api 23] en adelante)
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.CAMERA),
+                requestCameraPermission
+            )
+        } else {
+            // si ha sido aceptado previamente (para las version inferiores a las 6.0 [api 23])
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, requestCameraPicture)
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            requestCameraPermission -> {
+                if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //permiso aceptado
+                    val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    startActivityForResult(intent, requestCameraPicture)
+
+                } else {
+                    // permiso denegado
+                    Toast.makeText(
+                        this,
+                        "you can't take a picture if you dan't allow it",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                }
+            }
+        }
+    }
+
+   /* override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            requestCameraPicture -> {
+                // comprobar si el resultado es bueno
+                if (resultCode == Activity.RESULT_OK) {
+                    // obtenemos los extras del intent recibido por parametros
+                    val extras = data!!.extras
+                    // formamos el bitmap a partir d elos extras
+                    val imageBipMap = extras.get("data") as Bitmap
+                    // cargamos la foto como bitmap en el imageView
+                    imageViewPicture.setImageBitmap(imageBipMap)
+                } else {
+                    // la foto no ha sido tomada con exito
+                    Toast.makeText(this, "Picture has failed!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }*/
+
+    @SuppressLint("MissingSuperCall")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            requestCameraPicture -> {
+                // Comprobar si el resultado es bueno
+                if (resultCode == Activity.RESULT_OK) {
+                    // Obtenemos los extras del intent recibido por parametros
+                    val extras = data!!.extras
+                    // Formamos el bitmap a partir de los extras
+                    val imageBitMap = extras.get("data") as Bitmap
+                    // Cargamos la foto como bitmap en el imageView
+                    imageViewPicture.setImageBitmap(imageBitMap)
+                } else {
+                    // La foto no ha sido tomada con éxito
+                    Toast.makeText(this, "Picture has failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
